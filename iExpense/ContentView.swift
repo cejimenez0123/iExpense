1f015a189e3767134f1ac862773761c44907acf9
cejimenez0123 <christianjimenez0123@gmail.com>
2021-01-24 11:37:04 -0500

    Button

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,13 +6,35 @@
 //
 
 import SwiftUI
-
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+struct ExpenseItem{
+    let name: String
+    let type: String
+    let amount: Int
+    
 }
+class Expenses: ObservableObject{
+    
+    @Published var items=[ExpenseItem]()
+}
+struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(expenses.items,id: \.name){ item in
+                    Text(item.name)
+                        }
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing: Button(action: {
+                    let expense = ExpenseItem(name: "Test", type: "Personal", amount: 5)
+                    self.expenses.items.append(expense)}){
+                
+                
+                Image(systemName: "plus")
+            })
+        }
+    }}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
