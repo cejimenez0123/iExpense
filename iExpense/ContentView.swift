93b3fdcd04bd666fb662dc674e39ca92aab92f3a
cejimenez0123 <christianjimenez0123@gmail.com>
2021-01-24 11:39:14 -0500

    id

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 struct ExpenseItem{
+    let id= UUID()
     let name: String
     let type: String
     let amount: Int
@@ -21,7 +22,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             List{
-                ForEach(expenses.items,id: \.name){ item in
+                ForEach(expenses.items,id: \.id){ item in
                     Text(item.name)
                         }
             }
